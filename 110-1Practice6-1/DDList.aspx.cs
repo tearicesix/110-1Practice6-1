c514c92c0019cf58d6f2ff622c57cfe8a89b5397
冠登 廖 <t8599008@ntut.org.tw>
2021-10-28 20:03:45 +0800

    Added

---
+++ 110-1Practice6-1/DDList.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice6_1 {
+    public partial class DDList : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+    }
+}
